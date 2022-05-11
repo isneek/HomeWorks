6aba688d2c0299b419927561cf17ca651ba42945
isneek <isneek1959@gmail.com>
2022-05-11 15:09:55 +0300

    add folder hw2

---
+++ hw2/script.js
@@ -0,0 +1,13 @@
+const userName = "Igor";
+const userSurname = "Irza";
+const userAge = 63;
+console.log(userName , userSurname , userAge);
+
+let newUser;
+let shoppingList;
+let isUserOnline;
+let employeeSalary;
+let cursorCoordinates;
+
+let greeting = "Hello World!";
+console.log(greeting);
