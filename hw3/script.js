0e62ec1d462d0e890ff153042e05b3478e154a7a
isneek <isneek1959@gmail.com>
2022-05-15 21:41:47 +0300

    add hw3

---
+++ hw3/script.js
@@ -0,0 +1,70 @@
+//task 1
+
+let firstNumber;
+let secondNumber;
+
+firstNumber = Number(prompt('Введите Первое число: '));
+alert('Вы ввели число: ' +firstNumber);
+console.log('firstNumber', firstNumber);
+
+
+secondNumber = Number(prompt('Введите Второе число: '));
+alert('Вы ввели число: ' +secondNumber);
+console.log('secondNumber', secondNumber);
+
+alert('Вы ввели числа: ' + firstNumber + ' и ' + secondNumber);
+
+
+if (firstNumber > secondNumber){
+    alert('Первое число больше второго');
+    console.log('Первое число больше второго');
+}
+if (firstNumber < secondNumber){
+    alert('Второе число больше первого');
+    console.log('Второе число больше первого');
+}
+if(firstNumber === secondNumber){
+    alert('Числа равны');
+    console.log('Числа равны');
+};
+
+//task 2
+
+let num1 = prompt('первое число');
+num1 = Number(num1);
+
+let num2 = prompt('второе число');
+num2 = Number(num2);
+
+
+let operator = prompt('оператор (/,+,*,-)');
+
+
+switch (operator) {
+    case '+':
+        alert (num1 + num2);
+        console.log(num1 + num2);
+        break;
+
+    case '-':
+        alert (num1 - num2);
+        console.log(num1 - num2);
+        break;
+
+    case '*':
+        alert (num1 * num2);
+        console.log(num1 * num2);
+        break;
+
+    case '/':
+        alert (num1 / num2);
+        console.log(num1 / num2);
+        break;
+}
+
+
+//task 3
+
+
+
+
