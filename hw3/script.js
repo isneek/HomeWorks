2255b8400538fb2174ff762b7be5c84ee73dc833
isneek <isneek1959@gmail.com>
2022-05-16 18:11:31 +0300

    add hw3

--- hw3/script.js
+++ hw3/script.js
@@ -1,32 +1,32 @@
-//task 1
+// //task 1
 
-let firstNumber;
-let secondNumber;
+// let firstNumber;
+// let secondNumber;
 
-firstNumber = Number(prompt('Введите Первое число: '));
-alert('Вы ввели число: ' +firstNumber);
-console.log('firstNumber', firstNumber);
-
-
-secondNumber = Number(prompt('Введите Второе число: '));
-alert('Вы ввели число: ' +secondNumber);
-console.log('secondNumber', secondNumber);
-
-alert('Вы ввели числа: ' + firstNumber + ' и ' + secondNumber);
+// firstNumber = Number(prompt('Введите Первое число: '));
+// alert('Вы ввели число: ' +firstNumber);
+// console.log('firstNumber', firstNumber);
 
 
-if (firstNumber > secondNumber){
-    alert('Первое число больше второго');
-    console.log('Первое число больше второго');
-}
-if (firstNumber < secondNumber){
-    alert('Второе число больше первого');
-    console.log('Второе число больше первого');
-}
-if(firstNumber === secondNumber){
-    alert('Числа равны');
-    console.log('Числа равны');
-};
+// secondNumber = Number(prompt('Введите Второе число: '));
+// alert('Вы ввели число: ' +secondNumber);
+// console.log('secondNumber', secondNumber);
+
+// alert('Вы ввели числа: ' + firstNumber + ' и ' + secondNumber);
+
+
+// if (firstNumber > secondNumber){
+//     alert('Первое число больше второго');
+//     console.log('Первое число больше второго');
+// }
+// if (firstNumber < secondNumber){
+//     alert('Второе число больше первого');
+//     console.log('Второе число больше первого');
+// }
+// if(firstNumber === secondNumber){
+//     alert('Числа равны');
+//     console.log('Числа равны');
+// };
 
 //task 2
 
@@ -60,11 +60,24 @@ switch (operator) {
         alert (num1 / num2);
         console.log(num1 / num2);
         break;
+
+    default:
+        alert ('incorrect');
 }
 
+ 
+// //task 3
 
-//task 3
+//     const MIN_AGE = 12;
+//     const MAX_AGE = 80;
+//     const minAge = 18;
+//     const maxAge = 60;
+//     const visitorAge = Number(prompt('Сколько Вам лет?'));
 
-
-
-
+//     if (visitorAge < MIN_AGE || visitorAge > MAX_AGE){
+//         console.log('Ваш возраст слишком мал или велик для посещения.');
+//     } else if (visitorAge <= minAge ||visitorAge >= maxAge){
+//         console.log('Вход разрешен с разрешения взрослых!');
+//     } else {
+//         console.log('Вход разрешен!');
+//     }
