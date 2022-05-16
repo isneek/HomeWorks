15b33870100441c7e2042784522c40ab43cf5dc7
isneek <isneek1959@gmail.com>
2022-05-17 01:13:15 +0300

    fixed hw3 #2

--- hw3/script.js
+++ hw3/script.js
@@ -1,71 +1,70 @@
-//task 1
+// //task 1
 
-let firstNumber;
-let secondNumber;
+// let firstNumber;
+// let secondNumber;
 
-firstNumber = Number(prompt('Введите Первое число: '));
-alert('Вы ввели число: ' +firstNumber);
-console.log('firstNumber', firstNumber);
-
-
-secondNumber = Number(prompt('Введите Второе число: '));
-alert('Вы ввели число: ' +secondNumber);
-console.log('secondNumber', secondNumber);
-
-alert('Вы ввели числа: ' + firstNumber + ' и ' + secondNumber);
+// firstNumber = Number(prompt('Введите Первое число: '));
+// alert('Вы ввели число: ' +firstNumber);
+// console.log('firstNumber', firstNumber);
 
 
-if (firstNumber > secondNumber){
-    alert('Первое число больше второго');
-    console.log('Первое число больше второго');
-}
-if (firstNumber < secondNumber){
-    alert('Второе число больше первого');
-    console.log('Второе число больше первого');
-}
-if(firstNumber === secondNumber){
-    alert('Числа равны');
-    console.log('Числа равны');
-};
+// secondNumber = Number(prompt('Введите Второе число: '));
+// alert('Вы ввели число: ' +secondNumber);
+// console.log('secondNumber', secondNumber);
 
-//task 2
-
-let num1 = prompt('первое число');
-num1 = Number(num1);
-
-let num2 = prompt('второе число');
-num2 = Number(num2);
+// alert('Вы ввели числа: ' +firstNumber + ' и ' +secondNumber);
 
 
-let operator = prompt('оператор (/,+,*,-)');
+// if (firstNumber > secondNumber){
+//     alert('Первое число больше второго');
+//     console.log('Первое число больше второго');
+// }
+// if (firstNumber < secondNumber){
+//     alert('Второе число больше первого');
+//     console.log('Второе число больше первого');
+// }
+// if(firstNumber === secondNumber){
+//     alert('Числа равны');
+//     console.log('Числа равны');
+// };
+
+// //task 2
+
+// let num1 = prompt('первое число');
+// num1 = Number(num1);
+
+// let num2 = prompt('второе число');
+// num2 = Number(num2);
+
+// let operator = prompt('оператор (/,+,*,-)');
 
 
-switch (operator) {
-    case '+':
-        alert (num1 + num2);
-        console.log(num1 + num2);
-        break;
+// switch (operator) {
+//     case '+':
+//         alert (num1 + num2);
+//         console.log(num1 + num2);
+//         break;
 
-    case '-':
-        alert (num1 - num2);
-        console.log(num1 - num2);
-        break;
+//     case '-':
+//         alert (num1 - num2);
+//         console.log(num1 - num2);
+//         break;
 
-    case '*':
-        alert (num1 * num2);
-        console.log(num1 * num2);
-        break;
+//     case '*':
+//         alert (num1 * num2);
+//         console.log(num1 * num2);
+//         break;
 
-    case '/':
-        alert (num1 / num2);
-        console.log(num1 / num2);
-        break;
+//     case '/':
+//         alert (num1 / num2);
+//         console.log(num1 / num2);
+//         break;
 
-    default:
-        alert ('incorrect');
-}
+//     default:
+//         alert ('incorrect');
+// }
 
- 
+
 //task 3
 
     const MIN_AGE = 12;
@@ -73,11 +72,16 @@ switch (operator) {
     const minAge = 18;
     const maxAge = 60;
     const visitorAge = Number(prompt('Сколько Вам лет?'));
+    const ageConfirmed = confirm(`Вам действительно ${visitorAge} лет?`);
 
-    if (visitorAge < MIN_AGE || visitorAge > MAX_AGE){
+    if (ageConfirmed){
+        alert('Информация сохранена.');
+    
+    } if (visitorAge < MIN_AGE || visitorAge > MAX_AGE){
         console.log('Ваш возраст слишком мал или велик для посещения.');
-    } else if (visitorAge <= minAge ||visitorAge >= maxAge){
+    } else if (visitorAge < minAge ||visitorAge >= maxAge){
         console.log('Вход разрешен с разрешения взрослых!');
     } else {
         console.log('Вход разрешен!');
     };
+    
