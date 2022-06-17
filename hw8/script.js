db0e0d9e1be3cf43a7f1db4cf1003af8690042aa
isneek <isneek1959@gmail.com>
2022-06-17 21:32:03 +0300

    add hw8

--- hw8/script.js
+++ hw8/script.js
@@ -1,68 +1,82 @@
 // task1
 
-// const getCountTrue = (arr) => {
-//     const count = [];
-//     for(let elem of arr)
-//     if (elem === true) {
-//         count.push(elem);
-//         console.log('elem', elem)
-//     }
-//     return count;
-// };
+const getCountTrue = (value) => {
+    const count = [];
 
-// const countTrue = getCountTrue([true, false, false, true, false]);
+    for(let elem of value)
+    if (elem === true) {
+        count.push(elem);
+        console.log('elem', elem)
+    }
+    return count.length;
+};
+const countTrue = getCountTrue([true, true, false, false, false]);
 
-// console.log('countTrue', countTrue)
+const countTrue1 = getCountTrue([false, false, false, false]);
+
+const countTrue2 = getCountTrue([]);
+
+console.log('countTrue', countTrue)
+console.log('countTrue1', countTrue1)
+console.log('countTrue2', countTrue2)
 
 
 // task2
 
-// const getOccurrencesCount = (arr) =>{
-// result = { };
-// for(let i = 0; i < arr.length; ++i) {
-//     if(!result[arr[i]]){
-//     result[arr[i]] = 0;
-//     }
-//     ++result[arr[i]];
-//     }
-// }
+const getOccurrencesCount = (arr) =>{
+let result = {};
+for(let i = 0; i < arr.length; i++) {
+    if(!result[arr[i]]){
+    result[arr[i]] = 0;
+    }
+    ++result[arr[i]];
+    }
+    console.log('result', result)
+    
+}
 
-// getOccurrencesCount(["a", "v", "a", "b", "b",]);
+getOccurrencesCount(["a", "v", "a", "b", "b",]);
 
-// console.log(result);
-
-// getOccurrencesCount([
-//     "apples",
-//     "oranges",
-//     "pears",
-//     "pears",
-//     "apples",
-//     "oranges",
-//     "oranges",
-//     "pears",
-//   ]);
-
-//     console.log(result);
+getOccurrencesCount([
+    "apples",
+    "oranges",
+    "pears",
+    "pears",
+    "apples",
+    "oranges",
+    "oranges",
+    "pears",
+  ]);
 
 
     // task3
 
 const getFindExcess = (arr) =>{
-    let evenNumbers = [];
-    let oddNumbers = [];
+
+    let evenNumber = [];
+    let oddNumber = [];
         
     for(let num of arr) {
         if (num % 2 === 0){
-            evenNumbers.push(num);
-            console.log(`evenNumbers`, evenNumbers)
-         } else {
-            oddNumbers.push(num);
-            console.log(`oddNumbers`, oddNumbers)
-         };
-    }
-}
 
-getFindExcess([1, 2, 3]);
-// getFindExcess([2, 6, 8, 10, 3]);
-// getFindExcess([1, 1, 0, 1, 1]);
+            evenNumber.push(num);
+        } else {
+            oddNumber.push(num);
+        };
+    }
+
+    if(evenNumber.length > oddNumber.length){ 
+        return oddNumber[0] 
+        }else{
+        return evenNumber[0]
+    }
+}   
+
+const number = getFindExcess([1, 2, 3]);
+const number1 = getFindExcess([2, 6, 8, 10, 3]);
+const number2 = getFindExcess([1, 1, 0, 1, 1]);
+
+console.log(`number`, number);
+console.log(`number1`, number1);
+console.log(`number2`, number2);
 
